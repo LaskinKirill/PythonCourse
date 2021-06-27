02a215ef1fc474dc11716190b16e4ab01cfe5ba5
Kirill Laskin <KirillLaskin@itcenterifmo.onmicrosoft.com>
2021-06-27 17:40:05 +0300

    Cesar

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,46 +1,67 @@
 import typing as tp
 
+llst = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф',
+            'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+blst = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф',
+            'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я']
 
-def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
-    """
-    Encrypts plaintext using a Caesar cipher.
+def encryptCaesar(msg, shift=3):
+    ret = ""
+    for x in msg:
+        if x in llst:
+            ind = llst.index(x)
+            ret += llst[ind + shift]
+        elif x in blst:
+            ind = blst.index(x)
+            ret += blst[ind + shift]
+        else:
+            ret += x
+    return ret
 
-    >>> encrypt_caesar("PYTHON")
-    'SBWKRQ'
-    >>> encrypt_caesar("python")
-    'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
-    >>> encrypt_caesar("")
-    ''
-    """
-    ciphertext = ""
-    # PUT YOUR CODE HERE
-    return ciphertext
-
-
-def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
-    """
-    Decrypts a ciphertext using a Caesar cipher.
-
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
-    """
-    plaintext = ""
-    # PUT YOUR CODE HERE
-    return plaintext
+##    ciphertext = ""
+##   # PUT YOUR CODE HERE
+##    return ciphertext
 
 
-def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
-    """
-    Brute force breaking a Caesar cipher.
-    """
-    best_shift = 0
-    # PUT YOUR CODE HERE
-    return best_shift
+def decryptCaesar(msg, shift=3):
+    ret = ""
+    for x in msg:
+        if x in llst:
+            ind = llst.index(x)
+            ret += llst[ind - shift]
+        elif x in blst:
+            ind = blst.index(x)
+            ret += blst[ind - shift]
+        else:
+            ret += x
+    return ret
+
+
+print(encryptCaesar("Да здравствует салат Цезарь!"))
+print(decryptCaesar("Зг кзугефхецих фгогх Щикгуя!"))
+
+
+##def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
+##    """
+##    Brute force breaking a Caesar cipher.
+##    """
+message = 'GIEWIVrGMTLIVrHIQS'  # encrypted message
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+for key in range(len(LETTERS)):
+    translated = ''
+    for symbol in message:
+        if symbol in LETTERS:
+            num = LETTERS.find(symbol)
+            num = num - key
+            if num < 0:
+                num = num + len(LETTERS)
+            translated = translated + LETTERS[num]
+        else:
+            translated = translated + symbol
+print('Hacking key #%s: %s' % (key, translated))
+
+
+##best_shift = 0
+# PUT YOUR CODE HERE
+##return best_shift
